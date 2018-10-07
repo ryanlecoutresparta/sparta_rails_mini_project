db3be4cc6b4ad69037c6e3b65d0f43a9813e5ef3
Tech-A07 <rlecoutre@spartaglobal.com>
2018-10-07 22:51:24 +0100

    enabled images to be added to active storage for users and tech

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
+    user.image.attach(params[:user][:prof_pic])
 
     respond_to do |format|
       if @user.save
--- app/models/tech.rb
+++ app/models/tech.rb
@@ -1,4 +1,9 @@
 class Tech < ApplicationRecord
+  validates :technology, presence: true
+  validates :date_created, presence: true
+  validates :version, presence: true
+  validates :desc, presence: true
+  validates :logo, presence: true
   belongs_to :user
   has_one_attached :logo
   has_many :comments, dependent: :destroy
